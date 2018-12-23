eac24a475cfb4a9007932733f9884c9f30758887
TakakiNishio <t2k4k0h61201@gmail.com>
2018-12-24 01:54:23 +0900

    [add] recent progresses

--- darknet_rospy/scripts/darknet_rospy.py
+++ darknet_rospy/scripts/darknet_rospy.py
@@ -15,9 +15,12 @@ import colorsys
 import numpy as np
 import os
 import shutil
-import argparse
+import copy
+# import argparse
 
 from darknet import *
+from darknet_rospy_msgs.msg import DarkBox
+from darknet_rospy_msgs.msg import DarkBoxArray
 
 
 class DarknetPyNode:
@@ -33,7 +36,13 @@ class DarknetPyNode:
         meta_file = file_path+rospy.get_param('~meta_file', "/../darknet/cfg/coco.data")
         names_file = file_path+rospy.get_param('~names_file', "/../darknet/data/coco.names")
         input_image_topic = rospy.get_param('~input_image_topic', "/camera/image_color")
-        output_image_topic = rospy.get_param('~output_image_topic', "/recognition_result")
+        output_image_topic = rospy.get_param('~output_image_topic', "/darknet/recognition_result_image")
+        output_topic = rospy.get_param('~output_topic', "/darknet/recognition_result")
+        cropping_area = rospy.get_param('~cropping_area', None)
+
+        if cropping_area is not None:
+            cropping_area = [[int(x) for x in ss.lstrip('[').split(' ')][0] for ss in cropping_area.rstrip(']').split(',')]
+        self.cropping_area = cropping_area
 
         if os.path.isdir('data'):
             shutil.rmtree('data')
@@ -47,6 +56,8 @@ class DarknetPyNode:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(input_image_topic, Image, self.image_callback, queue_size=1, buff_size=2**24)
         self.result_image_pub = rospy.Publisher(output_image_topic, Image, queue_size=1)
+        self.result_pub = rospy.Publisher(output_topic, DarkBoxArray, queue_size=1)
+
         self.color = (0,255,0)
         rospy.loginfo("Initialized DarknetPyNode")
 
@@ -58,17 +69,50 @@ class DarknetPyNode:
             print(e)
 
     def darknet_detection(self, cv_image):
+
+        # cv_image = cv2.resize(cv_image, None, fx=0.5, fy=0.5)
+
+        darkbox_array = DarkBoxArray()
+        darkbox_list = []
+
+        cv_image_ = copy.deepcopy(cv_image)
+        
+        if self.cropping_area is not None:
+            cv_image = cv_image[self.cropping_area[0]:self.cropping_area[1],
+                                self.cropping_area[2]:self.cropping_area[3]]
+
         r = detect_np(self.net, self.meta, cv_image)
         for i in r:
             x, y, w, h = i[2][0], i[2][1], i[2][2], i[2][3]
             xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
+            probability = round(i[1], 4)
+            label = i[0].decode()
+
+            if self.cropping_area is not None:
+                xmin += self.cropping_area[2]
+                xmax += self.cropping_area[2]
+                ymin += self.cropping_area[0]
+                ymax += self.cropping_area[0]
+
+            darkbox = DarkBox()
+            darkbox.probability = probability
+            darkbox.label = label
+            darkbox.xmin = xmin
+            darkbox.ymin = ymin
+            darkbox.xmax = xmax
+            darkbox.ymax = ymax
+
             pt1 = (xmin, ymin)
             pt2 = (xmax, ymax)
-            cv2.rectangle(cv_image, pt1, pt2, self.color, 2)
-            cv2.putText(cv_image, i[0].decode() + " [" + str(round(i[1] * 100, 2)) + "%]",
+            cv2.rectangle(cv_image_, pt1, pt2, self.color, 2)
+            cv2.putText(cv_image_, label + " [" + str(probability*100) + "%]",
                         (pt1[0]+2, pt1[1]+25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, self.color, 3)
 
-        self.result_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            darkbox_list.append(darkbox)
+
+        darkbox_array.dark_array = darkbox_list
+        self.result_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_, "bgr8"))
+        self.result_pub.publish(darkbox_array)
 
 
 if __name__ == "__main__":
